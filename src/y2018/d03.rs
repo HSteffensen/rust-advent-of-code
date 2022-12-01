c6941203778418f3affd82b2cbc46c956c548af2
HSteffensen <henry@steffensenfamily.com>
2022-12-01 08:24:25 +0100

    partial 2018d3p2

--- src/y2018/d03.rs
+++ src/y2018/d03.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use regex::Regex;
 
@@ -16,24 +16,13 @@ impl AocSolution for Part1 {
     const PART: u32 = 1;
 
     fn implementation(input: &str) -> String {
-        let mut fabric = HashMap::new();
-        let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
-        for matches in re.captures_iter(input) {
-            let _id: u32 = matches[1].parse().unwrap();
-            let x_0: u32 = matches[2].parse().unwrap();
-            let y_0: u32 = matches[3].parse().unwrap();
-            let dx: u32 = matches[4].parse().unwrap();
-            let dy: u32 = matches[5].parse().unwrap();
-
-            for x in x_0..(x_0 + dx) {
-                for y in y_0..(y_0 + dy) {
-                    fabric.entry((x, y)).and_modify(|v| *v += 1).or_insert(1);
-                }
-            }
-        }
+        let fabric = parse_fabric_claims(input);
         fabric
             .into_iter()
-            .fold(0, |count, (_, v)| if v > 1 { count + 1 } else { count })
+            .fold(
+                0,
+                |count, (_, v)| if v.len() > 1 { count + 1 } else { count },
+            )
             .to_string()
     }
 }
@@ -43,9 +32,39 @@ impl AocSolution for Part2 {
     const DAY: u32 = D;
     const PART: u32 = 2;
 
-    fn implementation(_input: &str) -> String {
+    fn implementation(input: &str) -> String {
+        let fabric = parse_fabric_claims(input);
+        let overlapping_claim_ids = HashSet::from_iter(
+            fabric
+                .iter()
+                .filter(|(_, v)| v.len() > 1)
+                .flat_map(|(_, v)| v),
+        );
+        // let all
         todo!()
     }
+}
+
+fn parse_fabric_claims(input: &str) -> HashMap<(u32, u32), Vec<u32>> {
+    let mut fabric = HashMap::new();
+    let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+    for matches in re.captures_iter(input) {
+        let id: u32 = matches[1].parse().unwrap();
+        let x_0: u32 = matches[2].parse().unwrap();
+        let y_0: u32 = matches[3].parse().unwrap();
+        let dx: u32 = matches[4].parse().unwrap();
+        let dy: u32 = matches[5].parse().unwrap();
+
+        for x in x_0..(x_0 + dx) {
+            for y in y_0..(y_0 + dy) {
+                fabric
+                    .entry((x, y))
+                    .and_modify(|v: &mut Vec<u32>| v.push(id))
+                    .or_insert(vec![id]);
+            }
+        }
+    }
+    fabric
 }
 
 #[test]
