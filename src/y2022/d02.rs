b58bb9f375749d03143d4d6321ac7699434d0a5a
Henry Steffensen <henry@steffensenfamily.com>
2022-12-02 09:09:42 +0100

    2022 d2 p2

--- src/y2022/d02.rs
+++ src/y2022/d02.rs
@@ -106,13 +106,60 @@ impl AocSolution for Part1 {
     }
 }
 
+fn desired_outcome(xyz: char) -> Outcome {
+    match xyz {
+        'X' => Outcome::Loss,
+        'Y' => Outcome::Tie,
+        'Z' => Outcome::Win,
+        _ => panic!("Unexpected self, must be X Y or Z, got: {}", xyz),
+    }
+}
+
+fn play_for_desired_outcome(opponent: &Shape, outcome: &Outcome) -> Shape {
+    match opponent {
+        Shape::Rock => match outcome {
+            Outcome::Win => Shape::Paper,
+            Outcome::Loss => Shape::Scissors,
+            Outcome::Tie => Shape::Rock,
+        },
+        Shape::Paper => match outcome {
+            Outcome::Win => Shape::Scissors,
+            Outcome::Loss => Shape::Rock,
+            Outcome::Tie => Shape::Paper,
+        },
+        Shape::Scissors => match outcome {
+            Outcome::Win => Shape::Rock,
+            Outcome::Loss => Shape::Paper,
+            Outcome::Tie => Shape::Scissors,
+        },
+    }
+}
+
+fn parse_input2(input: &str) -> Vec<(Shape, Outcome)> {
+    let mut parsed = Vec::new();
+    for line in input.lines() {
+        let opponent = opponent_shape(line.chars().next().unwrap());
+        let me = desired_outcome(line.chars().nth(2).unwrap());
+        parsed.push((opponent, me));
+    }
+    parsed
+}
+
 impl AocSolution for Part2 {
     const YEAR: u32 = Y;
     const DAY: u32 = D;
     const PART: u32 = 2;
 
     fn implementation(input: &str) -> String {
-        todo!()
+        let rounds = parse_input2(input);
+        let total_score: u32 = rounds
+            .iter()
+            .map(|(opponent, outcome)| {
+                let me = play_for_desired_outcome(opponent, outcome);
+                round_score(&me, outcome)
+            })
+            .sum();
+        total_score.to_string()
     }
 }
 
