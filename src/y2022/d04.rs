b8731fed275f400096939a1b76e1fdb38d2d76bf
HSteffensen <henry@steffensenfamily.com>
2022-12-04 21:34:20 +0100

    2022 d4

---
+++ src/y2022/d04.rs
@@ -0,0 +1,88 @@
+use std::ops::RangeInclusive;
+
+use regex::Regex;
+
+use crate::common::solution::AocSolution;
+
+use super::Y;
+
+struct Part1 {}
+struct Part2 {}
+const D: u32 = 4;
+
+fn parse_ranges(input: &str) -> Vec<(u32, u32, u32, u32)> {
+    let mut ranges = Vec::new();
+    let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
+    for matches in re.captures_iter(input) {
+        let a: u32 = matches[1].parse().unwrap();
+        let b: u32 = matches[2].parse().unwrap();
+        let c: u32 = matches[3].parse().unwrap();
+        let d: u32 = matches[4].parse().unwrap();
+        ranges.push((a, b, c, d));
+    }
+    ranges
+}
+
+fn range_fully_contains(outer: &RangeInclusive<&u32>, inner_start: &u32, inner_end: &u32) -> bool {
+    outer.contains(&inner_start) && outer.contains(&inner_end)
+}
+
+fn range_partially_contains(
+    outer: &RangeInclusive<&u32>,
+    inner_start: &u32,
+    inner_end: &u32,
+) -> bool {
+    outer.contains(&inner_start) || outer.contains(&inner_end)
+}
+
+impl AocSolution for Part1 {
+    const YEAR: u32 = Y;
+    const DAY: u32 = D;
+    const PART: u32 = 1;
+
+    fn implementation(input: &str) -> String {
+        parse_ranges(input)
+            .iter()
+            .filter(|(a, b, c, d)| {
+                range_fully_contains(&(a..=b), c, d) || range_fully_contains(&(c..=d), a, b)
+            })
+            .count()
+            .to_string()
+    }
+}
+
+impl AocSolution for Part2 {
+    const YEAR: u32 = Y;
+    const DAY: u32 = D;
+    const PART: u32 = 2;
+
+    fn implementation(input: &str) -> String {
+        parse_ranges(input)
+            .iter()
+            .filter(|(a, b, c, d)| {
+                range_partially_contains(&(a..=b), c, d) || range_partially_contains(&(c..=d), a, b)
+            })
+            .count()
+            .to_string()
+    }
+}
+
+#[test]
+fn p1_pull_examples() {
+    Part1::get_examples();
+}
+
+#[test]
+fn p1_run() {
+    Part1::solve();
+}
+
+#[test]
+fn p2_pull_examples() {
+    Part2::get_examples();
+}
+
+#[test]
+fn p2_run() {
+    Part2::solve();
+}
--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -1,5 +1,6 @@
 mod d01;
 mod d02;
 mod d03;
+mod d04;
 
 const Y: u32 = 2022;
