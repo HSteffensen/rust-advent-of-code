bc3cceeb814b568a5c9f003c6d34a6b7c052eb37
HSteffensen <henry@steffensenfamily.com>
2023-06-18 15:06:00 +0200

    2022 d17 p2

--- src/y2022/d17.rs
+++ src/y2022/d17.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, ops::Shr};
+use std::{collections::HashMap, fmt::Display, ops::Shr};
 
 use itertools::Itertools;
 
@@ -43,14 +43,17 @@ fn blocks() -> Vec<Vec<u16>> {
 
 struct TetrisGrid {
     grid: Vec<u16>,
+    blocks: Vec<Vec<u16>>,
+    block_index: usize,
+    jets: Vec<JetDirection>,
+    jet_index: usize,
 }
 
-impl Default for TetrisGrid {
-    fn default() -> Self {
-        Self {
-            grid: vec![0b111111111],
-        }
-    }
+#[derive(PartialEq, Eq, Hash)]
+struct GridHash {
+    block_index: usize,
+    jet_index: usize,
+    grid_top: [u16; 13],
 }
 
 impl Display for TetrisGrid {
@@ -69,6 +72,16 @@ impl Display for TetrisGrid {
 }
 
 impl TetrisGrid {
+    fn new(jets: Vec<JetDirection>) -> Self {
+        Self {
+            grid: vec![0b111111111],
+            blocks: blocks(),
+            block_index: 0,
+            jets,
+            jet_index: 0,
+        }
+    }
+
     fn height(&self) -> usize {
         self.grid
             .iter()
@@ -85,125 +98,95 @@ impl TetrisGrid {
         }
     }
 
-    fn check_collision(&self, block: &[u16], x: u8, y: usize) -> bool {
+    fn check_collision(&self, x: u8, y: usize) -> bool {
         self.grid
             .iter()
             .skip(y)
-            .zip(block.iter())
+            .zip(self.blocks.get(self.block_index).unwrap().iter())
             .any(|(g, b)| g & (b.shr(x)) != 0)
     }
 
-    fn set_block(&mut self, block: &Vec<u16>, x: u8, y: usize) {
+    fn set_block(&mut self, x: u8, y: usize) {
+        let block = self.blocks.get(self.block_index).unwrap();
         (0..block.len()).for_each(|i| {
             self.grid[y + i] |= block[i].shr(x);
         });
     }
 
-    fn fall_new_block(
-        &mut self,
-        block: &Vec<u16>,
-        stream: &mut dyn Iterator<Item = &JetDirection>,
-    ) {
+    fn next_block(&mut self) -> Vec<u16> {
+        self.block_index += 1;
+        self.block_index %= self.blocks.len();
+        self.blocks.get(self.block_index).unwrap().to_owned()
+    }
+
+    fn next_jet(&mut self) -> &JetDirection {
+        self.jet_index += 1;
+        self.jet_index %= self.jets.len();
+        self.jets.get(self.jet_index).unwrap()
+    }
+
+    fn fall_new_block(&mut self) {
         let mut block_y = self.height() + 4;
         let mut block_x: u8 = 3;
+        let block = self.next_block();
         self.extend_height(block_y + block.len() - 1);
-        for jet in stream {
+        loop {
+            let jet = self.next_jet();
             let new_x = match jet {
                 JetDirection::Left => block_x - 1,
                 JetDirection::Right => block_x + 1,
             };
             block_x = if (jet != &JetDirection::Left || block_x != 0)
-                && !self.check_collision(block, new_x, block_y)
+                && !self.check_collision(new_x, block_y)
             {
                 new_x
             } else {
                 block_x
             };
             let new_y = block_y - 1;
-            if self.check_collision(block, block_x, new_y) {
+            if self.check_collision(block_x, new_y) {
                 break;
             } else {
                 block_y = new_y;
             }
         }
-        self.set_block(block, block_x, block_y);
+        self.set_block(block_x, block_y);
+    }
+
+    fn grid_hash(&self) -> GridHash {
+        let mut grid = self.grid.iter().rev();
+        let grid_top = [
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+            grid.next().unwrap_or(&0).to_owned(),
+        ];
+        GridHash {
+            block_index: self.block_index,
+            jet_index: self.jet_index,
+            grid_top,
+        }
     }
 }
 
 #[test]
-fn test_tetris_block() {
-    let mut grid = TetrisGrid::default();
-    assert_eq!(grid.grid.len(), 1);
-    assert_eq!(grid.height(), 0);
-    grid.extend_height(3);
-    assert_eq!(grid.grid.len(), 4);
-    assert_eq!(grid.height(), 0);
-    assert_eq!(
-        grid.grid,
-        vec![0b111111111, 0b100000001, 0b100000001, 0b100000001]
-    );
-    let block = vec![0b10000000, 0b10000000];
-    grid.set_block(&block, 1, 1);
-    assert_eq!(
-        grid.grid,
-        vec![0b111111111, 0b101000001, 0b101000001, 0b100000001]
-    );
-    assert_eq!(grid.height(), 2);
-    let block = vec![0b01000000, 0b11100000, 0b01000000];
-    grid.extend_height(6);
-    grid.set_block(&block, 2, 3);
-    assert_eq!(
-        grid.grid,
-        vec![
-            0b111111111,
-            0b101000001,
-            0b101000001,
-            0b100010001,
-            0b100111001,
-            0b100010001,
-            0b100000001
-        ]
-    );
-    assert_eq!(grid.height(), 5);
-}
-
-#[test]
-fn test_collision() {
-    let grid = TetrisGrid {
-        grid: vec![
-            0b111111111,
-            0b101010101,
-            0b100010001,
-            0b100000001,
-            0b100000001,
-        ],
-    };
-    let block = vec![0b100000000, 0b100000000];
-    (0..7).step_by(2).for_each(|x| {
-        assert!(grid.check_collision(&block, x, 1));
-        assert!(!grid.check_collision(&block, x + 1, 1));
-        assert!(grid.check_collision(&block, x, 0));
-        assert!(grid.check_collision(&block, x + 1, 0));
-    });
-    assert!(grid.check_collision(&block, 8, 1));
-    assert!(grid.check_collision(&block, 8, 0));
-    assert!(grid.check_collision(&block, 0, 3));
-    assert!(grid.check_collision(&block, 8, 3));
-    (1..8).for_each(|x| {
-        assert!(!grid.check_collision(&block, x, 3), "{}", x);
-    });
-}
-
-#[test]
 fn test_falling_block() {
-    let mut grid = TetrisGrid::default();
     let input = parse_input(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>");
-    let mut stream = input.iter().cycle();
-    let binding = blocks();
-    let mut blocks = binding.iter().cycle();
+    let mut grid = TetrisGrid::new(input);
+    grid.block_index = grid.blocks.len() - 1;
+    grid.jet_index = grid.jets.len() - 1;
 
     assert_eq!(grid.grid, vec![0b111111111]);
-    grid.fall_new_block(blocks.next().unwrap(), &mut stream);
+    grid.fall_new_block();
     assert_eq!(
         grid.grid,
         vec![
@@ -214,7 +197,7 @@ fn test_falling_block() {
             0b100000001,
         ]
     );
-    grid.fall_new_block(blocks.next().unwrap(), &mut stream);
+    grid.fall_new_block();
     assert_eq!(
         grid.grid,
         vec![
@@ -228,6 +211,34 @@ fn test_falling_block() {
             0b100000001,
         ]
     );
+}
+
+fn solve(grid: &mut TetrisGrid, n: u64) -> usize {
+    let mut cache: HashMap<GridHash, u64> = HashMap::new();
+    let mut height_history: HashMap<u64, usize> = HashMap::new();
+    for i in 0..n {
+        grid.fall_new_block();
+        let state = grid.grid_hash();
+        let height = grid.height();
+        if let Some(last_index) = cache.get(&state) {
+            let last_height = height_history.get(last_index).unwrap();
+            let height_delta = height - last_height;
+            let drop_delta = i - last_index;
+            let drops_remaining = n - i;
+            let loops = drops_remaining / drop_delta;
+            let drops_after_loops = drops_remaining % drop_delta;
+            let height_after_loops = height + (height_delta * loops as usize);
+            let height_during_loop = height_history
+                .get(&(last_index + drops_after_loops - 1))
+                .unwrap();
+            let final_height = height_after_loops + height_during_loop - last_height;
+            return final_height;
+        } else {
+            cache.insert(state, i);
+        }
+        height_history.insert(i, height);
+    }
+    unreachable!()
 }
 
 impl AocSolution for Part1 {
@@ -236,21 +247,12 @@ impl AocSolution for Part1 {
     const PART: u32 = 1;
 
     fn implementation(input: &str) -> String {
-        let mut grid = TetrisGrid::default();
         let input = parse_input(input);
-        let mut stream = input.iter().cycle();
-        let binding = blocks();
-        let blocks = binding.iter().cycle();
+        let mut grid = TetrisGrid::new(input);
+        grid.block_index = grid.blocks.len() - 1;
+        grid.jet_index = grid.jets.len() - 1;
 
-        for (i, block) in blocks.enumerate() {
-            if i < 2022 {
-                grid.fall_new_block(block, &mut stream);
-            } else {
-                break;
-            }
-        }
-
-        grid.height().to_string()
+        solve(&mut grid, 2022).to_string()
     }
 }
 
@@ -260,7 +262,12 @@ impl AocSolution for Part2 {
     const PART: u32 = 2;
 
     fn implementation(input: &str) -> String {
-        todo!()
+        let input = parse_input(input);
+        let mut grid = TetrisGrid::new(input);
+        grid.block_index = grid.blocks.len() - 1;
+        grid.jet_index = grid.jets.len() - 1;
+
+        solve(&mut grid, 1000000000000).to_string()
     }
 }
 
