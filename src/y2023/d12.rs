bc1297fceb642f936b30e5532b8ee535ca19ca26
HSteffensen <henry@steffensenfamily.com>
2023-12-12 21:31:15 +0100

    2023 d12

--- src/y2023/d12.rs
+++ src/y2023/d12.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash, iter::repeat};
+
 use itertools::Itertools;
 use nom::{
     bytes::complete::tag,
@@ -14,12 +16,14 @@ use crate::common::solution::AocSolution;
 struct Part1 {}
 struct Part2 {}
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum SpringRecord {
     Damaged,
     Empty,
     Unknown,
 }
 
+#[derive(Debug)]
 struct RecordRow {
     springs: Vec<SpringRecord>,
     cluster_sizes: Vec<u32>,
@@ -68,6 +72,133 @@ impl RecordRow {
             .filter(|line| regex.is_match(line))
             .count()
     }
+
+    fn count_2(&self) -> usize {
+        let mut cache: HashMap<(usize, Option<SpringRecord>, usize, u32, bool), usize> =
+            HashMap::new();
+        self.count_2_helper(0, None, 0, 0, false, &mut cache)
+    }
+
+    fn count_2_helper<'a, 'b>(
+        &self,
+        spring_index: usize,
+        force_spring: Option<SpringRecord>,
+        cluster_index: usize,
+        cluster_count: u32,
+        in_cluster: bool,
+        cache: &mut HashMap<(usize, Option<SpringRecord>, usize, u32, bool), usize>,
+    ) -> usize {
+        if spring_index == self.springs.len() {
+            return if cluster_index == self.cluster_sizes.len()
+                || (cluster_index == (self.cluster_sizes.len() - 1)
+                    && cluster_count == self.cluster_sizes[cluster_index])
+            {
+                1
+            } else {
+                0
+            };
+        }
+        if cluster_index == self.cluster_sizes.len() {
+            return if self
+                .springs
+                .iter()
+                .skip(spring_index)
+                .all(|s| s == &SpringRecord::Empty || s == &SpringRecord::Unknown)
+            {
+                1
+            } else {
+                0
+            };
+        }
+        let cache_key = (
+            spring_index,
+            force_spring,
+            cluster_index,
+            cluster_count,
+            in_cluster,
+        );
+        if let Some(&x) = cache.get(&cache_key) {
+            return x;
+        }
+        let current_spring = force_spring.unwrap_or(self.springs[spring_index]);
+        let answer = match current_spring {
+            SpringRecord::Damaged => {
+                if cluster_count < self.cluster_sizes[cluster_index] {
+                    self.count_2_helper(
+                        spring_index + 1,
+                        None,
+                        cluster_index,
+                        cluster_count + 1,
+                        true,
+                        cache,
+                    )
+                } else {
+                    0
+                }
+            }
+            SpringRecord::Empty => {
+                if in_cluster {
+                    if cluster_count == self.cluster_sizes[cluster_index] {
+                        self.count_2_helper(
+                            spring_index + 1,
+                            None,
+                            cluster_index + 1,
+                            0,
+                            false,
+                            cache,
+                        )
+                    } else {
+                        0
+                    }
+                } else {
+                    self.count_2_helper(
+                        spring_index + 1,
+                        None,
+                        cluster_index,
+                        cluster_count,
+                        in_cluster,
+                        cache,
+                    )
+                }
+            }
+            SpringRecord::Unknown => {
+                self.count_2_helper(
+                    spring_index,
+                    Some(SpringRecord::Damaged),
+                    cluster_index,
+                    cluster_count,
+                    in_cluster,
+                    cache,
+                ) + self.count_2_helper(
+                    spring_index,
+                    Some(SpringRecord::Empty),
+                    cluster_index,
+                    cluster_count,
+                    in_cluster,
+                    cache,
+                )
+            }
+        };
+        cache.insert(cache_key, answer);
+        answer
+    }
+
+    fn as_part_2(self) -> RecordRow {
+        let mut springs = self.springs;
+        springs.push(SpringRecord::Unknown);
+        let mut springs = repeat(springs).take(5).flatten().collect_vec();
+        springs.pop();
+        RecordRow {
+            springs,
+            cluster_sizes: repeat(self.cluster_sizes).take(5).flatten().collect_vec(),
+        }
+    }
+}
+
+#[test]
+fn test_count_2() {
+    assert_eq!(1, parse_line("???.### 1,1,3").count_2());
+    assert_eq!(10, parse_line("?###???????? 3,2,1").count_2());
 }
 
 fn parse_line(line: &str) -> RecordRow {
@@ -101,7 +232,7 @@ impl AocSolution for Part1 {
         input
             .lines()
             .map(parse_line)
-            .map(|l| l.count_possibilities())
+            .map(|l| l.count_2())
             .sum::<usize>()
             .to_string()
     }
@@ -114,7 +245,12 @@ impl AocSolution for Part2 {
     }
 
     fn implementation(input: &str) -> String {
-        todo!("{}", input)
+        input
+            .lines()
+            .map(parse_line)
+            .map(|l| l.as_part_2().count_2())
+            .sum::<usize>()
+            .to_string()
     }
 }
 
